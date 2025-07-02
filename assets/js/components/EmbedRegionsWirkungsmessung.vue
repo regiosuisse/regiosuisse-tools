74a7bac23b37aa7a99a1b535447e220ae22bf082
Sidney Bösch <sidney.boesch@regiosuisse.ch>
2025-07-02 14:32:05 +0200

    fix translations

--- assets/js/components/EmbedRegionsWirkungsmessung.vue
+++ assets/js/components/EmbedRegionsWirkungsmessung.vue
@@ -1775,8 +1775,8 @@ export default {
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>
                         </div>
                         <div class="embed-regions-content-map-marker-overlay">
-                            <p><strong>${project.name}</strong><br>${this.$t('Jahr')} ${project.year}</p>
-                            ${project.link ? `<p><a href="${project.link}" target="_blank">${this.$t('Zum Kurzbericht')}</a></p>` : ``}
+                            <p><strong>${project.name}</strong><br>${this.$t('Jahr', this.locale)} ${project.year}</p>
+                            ${project.link ? `<p><a href="${project.link}" target="_blank">${this.$t('Zum Kurzbericht', this.locale)}</a></p>` : ``}
                         </div>
                     `;
                     if(parseInt(project.year) >= 2020) {
